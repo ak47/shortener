c8ad0623f9acb859f973ed1b48bf240e454a307e
Andy Koch <developer@no-ego.net>
2012-01-14 12:29:01 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shortener::Application.config.secret_token = '7b6d5ea6aafc43abc6d8ad8ba0186f5cde7e352dc4e40841aa5275a44a16da522c4e6890db74fa81bee986dca90c79fe545f92861404b4a87c4ea5b111a178ca'
