38f4dcd484d50d941dfdf217cde1f24e84c204e4
Andy Koch <developer@no-ego.net>
2012-01-14 15:32:06 -0800

    implement url hash mechanism
    Going with crc32, not an optimal solution, but sufficient for a proof-of-concept level product.

--- app/models/shorty.rb
+++ app/models/shorty.rb
@@ -1,3 +1,11 @@
+require 'zlib'
+
 class Shorty < ActiveRecord::Base
+  include Zlib
+
   validates :url, :presence => true
+
+  before_save do |shorty|
+    shorty.key = crc32(shorty.url)
+  end
 end
--- spec/models/shorty_spec.rb
+++ spec/models/shorty_spec.rb
@@ -4,8 +4,22 @@ describe Shorty do
   it "is valid from the factory" do
     Factory.create(:shorty).should be_valid
   end
-  
+
   it "requires a url" do
     Factory.build(:shorty, :url => '').should_not be_valid
   end
+
+  describe "a key is generated from url attribute" do
+    it "for persisted objects" do
+      Factory.create(:shorty).key.should_not be_blank
+    end
+
+    it "and updates when object url changes" do
+      shorty = Factory.create(:shorty)
+      orig_key = shorty.key
+      shorty.update_attributes(:url => 'boomba')
+      orig_key.should_not == shorty.key
+    end
+  end
+
 end
