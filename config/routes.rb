7ae87fed6a7d0fbbf2e293718dd819c4a906e358
Andy Koch <developer@no-ego.net>
2012-01-14 17:02:02 -0800

    add convenience name for root redirector route add convenience test link to shorty view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Shortener::Application.routes.draw do
   resource "redirecter", :only => [:show]
   resources :shorties
-  match '/:key' => 'redirecters#show'
+  match '/:key' => 'redirecters#show', :as => :root_directer
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
